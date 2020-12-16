2b222094fa97f4d28ad41ae3da16fa2506c70221
dj51401 <dj51401@gmail.com>
2020-12-16 12:37:42 -0700

    Renamed and redesigned

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+
 function calculateDebt(){
+    google.charts.load("current", {"packages":["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
     var downPayment = -(document.getElementById("down-payment-input").value / 100) + 1;
     var loanAmount = document.getElementById("amount-input").value * downPayment;
     var interestRate = (document.getElementById("interest-input").value / 100) / 12;
@@ -17,4 +20,23 @@ function calculateDebt(){
 
     var totalLoan = (mortgagePayment + propertyTax + homeInsurance) * 1.1;
     document.getElementById("total-payment-text").innerHTML = "$" + totalLoan.toFixed();
+
+    function drawChart(){
+
+        var data = google.visualization.arrayToDataTable([
+            ["Bill", "Price"],
+            ["Mortgage Payment", mortgagePayment - (interestPaid / loanLength)],
+            ['Property Tax', propertyTax],
+            ['Home Insurance', homeInsurance],
+            ['Interest Paid', interestPaid / loanLength]
+        ]);
+
+        var options = {
+            title: 'Payment Chart'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById("piechart"));
+
+        chart.draw(data, options);
+    }
 }
